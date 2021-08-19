8906c21f790515c7e40549426c1d2ac33d0aeb13
dmitrii-bogdanov <d.bogdanov@spbu.ru>
2021-08-19 10:50:41 +0300

    Initial commit

---
+++ src/test/java/bogdanov/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package bogdanov.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

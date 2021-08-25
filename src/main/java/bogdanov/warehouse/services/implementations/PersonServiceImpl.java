1f4cf0c14eeb714bb60b1d7ac74423d938619b42
dmitrii-bogdanov <d.bogdanov@spbu.ru>
2021-08-25 15:26:29 +0300

    Changes to PersonRepository

--- src/main/java/bogdanov/warehouse/services/implementations/PersonServiceImpl.java
+++ src/main/java/bogdanov/warehouse/services/implementations/PersonServiceImpl.java
@@ -1,12 +1,89 @@
 package bogdanov.warehouse.services.implementations;
 
+import bogdanov.warehouse.dto.PersonDTO;
 import bogdanov.warehouse.services.interfaces.PersonService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 //TODO
 @Service
 public class PersonServiceImpl implements PersonService {
 
 
+    @Override
+    public PersonDTO add(PersonDTO person) {
+        return null;
+    }
 
+    @Override
+    public List<PersonDTO> add(List<PersonDTO> persons) {
+        return null;
+    }
+
+    @Override
+    public PersonDTO update(PersonDTO person) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> update(List<PersonDTO> person) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllByFirstname(String firstname) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllByLastname(String lastname) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllByPatronymic(String patronymic) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllByBirthDate(LocalDate date) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllOlderThan(int age) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllYoungerThan(int age) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllByPhoneNumber(String phoneNumber) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllByPhoneNumberStartingWith(String startWith) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<PersonDTO> findAllByEmailContaining(String partialEmail) {
+        return null;
+    }
 }
--- src/main/java/bogdanov/warehouse/services/interfaces/PersonService.java
+++ src/main/java/bogdanov/warehouse/services/interfaces/PersonService.java
@@ -1,8 +1,44 @@
 package bogdanov.warehouse.services.interfaces;
 
+import bogdanov.warehouse.database.entities.PersonEntity;
+import bogdanov.warehouse.dto.PersonDTO;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 //TODO
 @Service
 public interface PersonService {
+
+    PersonDTO add(PersonDTO person);
+
+    List<PersonDTO> add(List<PersonDTO> persons);
+
+    PersonDTO update(PersonDTO person);
+
+    List<PersonDTO> update(List<PersonDTO> person);
+
+    List<PersonDTO> getAll();
+
+    List<PersonDTO> findAllByFirstname(String firstname);
+
+    List<PersonDTO> findAllByLastname(String lastname);
+
+    List<PersonDTO> findAllByPatronymic(String patronymic);
+
+    List<PersonDTO> findAllByBirthDate(LocalDate date);
+
+    List<PersonDTO> findAllOlderThan(int age);
+
+    List<PersonDTO> findAllYoungerThan(int age);
+
+    List<PersonDTO> findAllByPhoneNumber(String phoneNumber);
+
+    List<PersonDTO> findAllByPhoneNumberStartingWith(String startWith);
+
+    List<PersonDTO> findAllByEmail(String email);
+
+    List<PersonDTO> findAllByEmailContaining(String partialEmail);
+
 }

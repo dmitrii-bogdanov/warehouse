570ddc3afef7d4b1e19f00f4dda8ff058ccdb6da
dmitrii-bogdanov <d.bogdanov@spbu.ru>
2021-09-13 11:33:41 +0300

    RecordTypeServiceTest

--- src/main/java/bogdanov/warehouse/services/implementations/cashed/RecordTypeServiceImpl.java
+++ src/main/java/bogdanov/warehouse/services/implementations/cashed/RecordTypeServiceImpl.java
@@ -4,39 +4,29 @@ import bogdanov.warehouse.database.entities.RecordTypeEntity;
 import bogdanov.warehouse.database.repositories.RecordTypeRepository;
 import bogdanov.warehouse.dto.RecordTypeDTO;
 import bogdanov.warehouse.exceptions.ArgumentException;
-import bogdanov.warehouse.exceptions.enums.ExceptionType;
 import bogdanov.warehouse.exceptions.ResourceNotFoundException;
+import bogdanov.warehouse.exceptions.enums.ExceptionType;
 import bogdanov.warehouse.services.interfaces.RecordTypeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@Primary
 @Service
+@RequiredArgsConstructor
 public class RecordTypeServiceImpl implements RecordTypeService {
 
     private final RecordTypeRepository recordTypeRepository;
     private final ObjectMapper objectMapper;
-    private final Map<String, RecordTypeEntity> entities = new HashMap<>();
 
     private static final String RECORD_TYPE = "RecordType";
     private static final String ID = "id";
     private static final String NAME = "name";
-
-    public RecordTypeServiceImpl(RecordTypeRepository recordTypeRepository,
-                                 ObjectMapper objectMapper) {
-        this.recordTypeRepository = recordTypeRepository;
-        this.objectMapper = objectMapper;
-        updateCache();
-    }
-
-    private void updateCache() {
-        entities.clear();
-        recordTypeRepository.findAll().forEach(e -> entities.put(e.getName(), e));
-    }
 
     private RecordTypeDTO convert(RecordTypeEntity entity) {
         return objectMapper.convertValue(entity, RecordTypeDTO.class);
@@ -44,7 +34,7 @@ public class RecordTypeServiceImpl implements RecordTypeService {
 
     @Override
     public List<RecordTypeDTO> getAll() {
-        return entities.values().stream().map(this::convert).toList();
+        return recordTypeRepository.findAll().stream().map(this::convert).toList();
     }
 
     @Override
@@ -57,7 +47,7 @@ public class RecordTypeServiceImpl implements RecordTypeService {
         if (id == null) {
             throw new ArgumentException(ExceptionType.NULL_ID);
         }
-        return entities.values().stream().filter(e -> e.getId() == id).findAny()
+        return recordTypeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(RECORD_TYPE, ID, id));
     }
 
@@ -67,17 +57,15 @@ public class RecordTypeServiceImpl implements RecordTypeService {
     }
 
     @Override
+    @Cacheable("record_types")
     public RecordTypeEntity getEntityByName(String name) {
         if (Strings.isBlank(name)) {
             throw new ArgumentException(ExceptionType.BLANK_ENTITY_NAME.setEntity(RecordTypeEntity.class));
         }
 
-        return Optional.ofNullable(entities.get(name.toUpperCase(Locale.ROOT)))
+        return recordTypeRepository.findByNameIgnoreCase(name)
                 .orElseThrow(() -> new ResourceNotFoundException(RECORD_TYPE, NAME, name.toUpperCase(Locale.ROOT)));
-//        if (entities.containsKey(name)) {
-//            return entities.get(name);
-//        } else {
-//            throw new ResourceNotFoundException(RECORD_TYPE, NAME, name);
-//        }
     }
+
+
 }

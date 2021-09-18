8e13c0ce8cbd61ffe9d16e8b351a0bdbab745d20
dmitrii-bogdanov <d.bogdanov@spbu.ru>
2021-09-18 10:20:05 +0300

    Corrections to ResourceNotFoundException

--- src/main/java/bogdanov/warehouse/exceptions/ResourceNotFoundException.java
+++ src/main/java/bogdanov/warehouse/exceptions/ResourceNotFoundException.java
@@ -6,7 +6,7 @@ import bogdanov.warehouse.exceptions.enums.ExceptionType;
 public class ResourceNotFoundException extends WarehouseExeption {
 
     public ResourceNotFoundException(String entity, String field, Object value) {
-        super(ExceptionType.RESOURCE_NOT_FOUND.setEntity(entity).setFieldName(field).setFieldValue(value));
+        this(ExceptionType.RESOURCE_NOT_FOUND.setEntity(entity).setFieldName(field).setFieldValue(value));
     }
 
     public ResourceNotFoundException(ExceptionType exceptionType) {
